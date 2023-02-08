cbb86e43cdb45427c7c5b35e8154a2b921b983b3
sahand <sahand.u99@gmail.com>
2023-02-08 20:43:04 +0330

    Incomme models and some beutification

--- web/models.py
+++ web/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,3 +8,14 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
+
+
+class Income(models.Model):
+    text = models.CharField(max_length=255)
+    date = models.DateField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}-{}".format(self.date, self.amount)
